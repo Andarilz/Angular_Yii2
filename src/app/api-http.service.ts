75278cf8e6ae72831e07f8c76ba3ed4bb0532f5c
Andarilz <tartarus@gmail.com>
2024-04-22 18:46:14 +0300

    feat: Add getAuthors method to ApiHttpService
    Show authors from api with ApiHttpService

--- src/app/api-http.service.ts
+++ src/app/api-http.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Book } from "./Interfaces/Book";
+import {Author} from "./Interfaces/Author";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class ApiHttpService {
 
   getBooks(): Observable<Book[]> {
    return this.http.get<Book[]>('http://localhost:8080/books');
+  }
+
+  getAuthors(): Observable<Author[]> {
+    return this.http.get<Author[]>('http://localhost:8080/authors');
   }
 }
--- src/app/components/authors-list/authors-list.component.ts
+++ src/app/components/authors-list/authors-list.component.ts
@@ -1,29 +1,39 @@
 
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {Book} from "../../Interfaces/Book";
 import {Author} from "../../Interfaces/Author";
+import {ApiHttpService} from "../../api-http.service";
 
 @Component({
   selector: 'app-authors-list',
   templateUrl: './authors-list.component.html',
   styleUrls: ['./authors-list.component.scss']
 })
-export class AuthorsListComponent {
+export class AuthorsListComponent implements OnInit{
 
   //конструктор с dependency injection
-  constructor(private router: Router) {
+  constructor(private router: Router, public ApiHttpService: ApiHttpService) {
+  }
+
+  authors: Author[] = [];
+
+  ngOnInit(): void {
+    this.ApiHttpService.getAuthors().subscribe(authors => {
+      console.log(authors);
+      this.authors = authors;
+    })
   }
 
   //Данные для таблицы книг
-  authors: Author[] = [
-    { id: 1, name: 'Author 1', books: [{ id: 1, title: 'Book 1', language: 'English', pages: 200, description: 'Description 1', genre: 'comedy' }], country: 'English', birth_year: 1990 },
-    { id: 1, name: 'Author 1', books: [ { id: 1, title: 'Book 1', language: 'English', pages: 200, description: 'Description 1', genre: 'comedy' }], country: 'English', birth_year: 1990 },
-    { id: 1, name: 'Author 1', books: [ { id: 1, title: 'Book 1', language: 'English', pages: 200, description: 'Description 1', genre: 'comedy' }], country: 'English', birth_year: 1990 },
-  ];
+  // authors: Author[] = [
+  //   { id: 1, name: 'Author 1', books: [{ id: 1, title: 'Book 1', language: 'English', pages: 200, description: 'Description 1', genre: 'comedy' }], country: 'English', birth_year: 1990 },
+  //   { id: 1, name: 'Author 1', books: [ { id: 1, title: 'Book 1', language: 'English', pages: 200, description: 'Description 1', genre: 'comedy' }], country: 'English', birth_year: 1990 },
+  //   { id: 1, name: 'Author 1', books: [ { id: 1, title: 'Book 1', language: 'English', pages: 200, description: 'Description 1', genre: 'comedy' }], country: 'English', birth_year: 1990 },
+  // ];
 
 
-  displayedColumns: string[] = ['name', 'books', 'country', 'borth_year'];
+  displayedColumns: string[] = ['name', 'country', 'borth_year'];
 
   redirectToAuthorDetail(authorId: string) {
     this.router.navigate(['/author', authorId]);
